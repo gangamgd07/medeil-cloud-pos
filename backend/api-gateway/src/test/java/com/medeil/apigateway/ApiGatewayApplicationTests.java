e1a3b13758507959b76c79eeee6b66ab321cf3e5
Gangamgd <ganga7mgd@gmail.com>
2026-07-03 21:22:44 +0530

    feat: api-gateway module

---
+++ backend/api-gateway/src/test/java/com/medeil/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medeil.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
